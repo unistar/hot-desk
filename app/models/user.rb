245c14b2619a1c65857ee6b4981c8ba1b741f684
Stein Wang <fwln111@gmail.com>
2019-03-03 17:50:43 +0800

    fix typo for array

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ApplicationRecord
 
   def cross_check_hit(params, to_update = 0)
     current_booking = []
-    if to_update > 0
+    if to_update.to_i > 0
       current_booking << Booking.find(to_update)
     end
-    items_to_check = bookings - [current_booking]
+    items_to_check = bookings - current_booking
     items_to_check.any? {|b| (params[:begin_date] >= b.begin_date && params[:begin_date] <= b.end_date) || 
 		       (params[:end_date] <= b.end_date && params[:end_date] >= b.begin_date)}
   end
